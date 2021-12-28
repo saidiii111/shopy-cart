fc3b61bc7aa72574a29511943a8f9894ca32aadb
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-12-28 14:57:01 +0100

    adding onLoad & Add functions

--- script.js
+++ script.js
@@ -9,8 +9,26 @@ let data = [
   { id: 4, name: "product4", desc: "standard quality" },
 ];
 
-/*function drawUi(items) {
-    items.forEach(function (ele) {
-        productContainer
-    })
-}*/
+function drawUi(items) {
+  items.forEach(function (ele) {
+    productContainer.appendChild = "<div>" + ele.name + "</div>";
+  });
+}
+
+// Load data
+window.onload = function () {
+  drawUi(data);
+};
+
+// Add Item
+productBtn.addEventListener("click", addItem),
+  function addItem() {
+    let productInput = document.getElementById("product-input");
+    // simple validation
+    if (productInput.value == "") alert("You Should Enter one Product at least");
+
+    let lastId = data.length ? data[data.length - 1].id : 1;
+    // Add to array
+    data.push({ name: productInput.value });
+    console.log(data);
+  };
